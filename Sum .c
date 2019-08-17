452d6dad5059c32d88aa7602bdee69d101d2f7fa
17CS104 <47842601+17CS104@users.noreply.github.com>
2019-08-17 08:22:37 +0530

    Create Sum .c

---
+++ Sum .c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+Void main ()
+{
+int a,b,sum;
+printf("enter the numbers to add");
+scanf("%d%d",&a,&b);
+sum=a+b;
+printf("print the sum of two numbers=%d\n,sum");
+}
